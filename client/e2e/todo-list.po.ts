a628e865568f3418b02a5ec1905107359b2cd10e
robe1606 <robe1606@morris.umn.edu>
2019-02-25 17:38:59 -0600

    Created todo-list.po.ts. This creates methods for testing to navigate the page.

---
+++ client/e2e/todo-list.po.ts
@@ -0,0 +1,63 @@
+import {browser, by, element, Key} from 'protractor';
+
+export class TodoPage {
+  navigateTo() {
+    return browser.get('/todos');
+  }
+
+  //http://www.assertselenium.com/protractor/highlight-elements-during-your-protractor-test-run/
+  highlightElement(byObject) {
+    function setStyle(element, style) {
+      const previous = element.getAttribute('style');
+      element.setAttribute('style', style);
+      setTimeout(() => {
+        element.setAttribute('style', previous);
+      }, 200);
+      return "highlighted";
+    }
+
+    return browser.executeScript(setStyle, element(byObject).getWebElement(), 'color: red; background-color: yellow;');
+  }
+
+  getTodoTitle() {
+    let title = element(by.id('todo-list-title')).getText();
+    this.highlightElement(by.id('todo-list-title'));
+
+    return title;
+  }
+
+  typeAOwner(owner: string) {
+    let input = element(by.id('todoOwner'));
+    input.click();
+    input.sendKeys(owner);
+  }
+
+  typeABody(body: string) {
+    let input = element(by.id('todoBody'));
+    input.click();
+    input.sendKeys(body);
+  }
+
+  typeACategory(category: string) {
+    let input = element(by.id('todoCategory'));
+    input.click();
+    input.sendKeys(category);
+  }
+
+  typeAStatus(status: string) {
+    let input = element(by.id('todoStatus'));
+    input.click();
+    input.sendKeys(status);
+  }
+
+  backspace(){
+    browser.actions().sendKeys(Key.BACK_SPACE).perform();
+  }
+
+  getUniqueTodo(category:string) {
+    let todo = element(by.id(category)).getText();
+    this.highlightElement(by.id(category));
+
+    return todo;
+  }
+}
